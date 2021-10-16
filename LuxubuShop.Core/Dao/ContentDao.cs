c1d9924eb6456bfe71fdcaa6dbf5a3ce18b46bd6
thanhit2612 <thanhit2612gmail.com>
2021-10-16 12:32:06 +0700

    Tạo chức năng quản lý bài viết

---
+++ LuxubuShop.Core/Dao/ContentDao.cs
@@ -0,0 +1,76 @@
+﻿using LuxubuShop.Core.EF;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuxubuShop.Core.Dao
+{
+	public class ContentDao
+	{
+		OnlineShopDbContext db = null;
+		public ContentDao()
+		{
+			db = new OnlineShopDbContext();
+		}
+		public Content GetContentByID(long id)
+		{
+			return db.Contents.Find(id);
+		}
+		// Get Method
+		public IEnumerable<Content> ListAllPaging(string searchString, int page, int pageSize)
+		{
+			IQueryable<Content> model = db.Contents;
+			if (!string.IsNullOrEmpty(searchString))
+			{
+				model = model.Where(x => x.Name.Contains(searchString));
+			}
+			return model.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pageSize);
+		}
+		// ViewDetail
+		public Content ViewDetail(int id)
+		{
+			return db.Contents.Find(id);
+		}
+		// Insert Method
+		public long Insert(Content entity)
+		{
+			db.Contents.Add(entity);
+			db.SaveChanges();
+			return entity.ID;
+		}
+		// Update Method
+		public bool Update(Content entity)
+		{
+			try
+			{
+				var content = db.Contents.Find(entity.ID);
+				content.Name = entity.Name;
+				db.SaveChanges();
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+		// Delete Method
+		public bool Delete(int id)
+		{
+			try
+			{
+				var content = db.Contents.Find(id);
+				db.Contents.Remove(content);
+				db.SaveChanges();
+				return true;
+
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+	}
+}
---
+++ LuxubuShop.Web/Areas/Admin/Controllers/ContentController.cs
@@ -0,0 +1,93 @@
+﻿using LuxubuShop.Core.Dao;
+using LuxubuShop.Core.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LuxubuShop.Web.Areas.Admin.Controllers
+{
+    public class ContentController : BaseController
+    {
+        // GET: Admin/Content
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 5)
+        {
+            var dao = new ContentDao();
+            var model = dao.ListAllPaging(searchString, page, pageSize);
+            ViewBag.SearchString = searchString;
+            SetViewBag();
+            return View(model);
+        }
+
+        public void SetViewBag(long? selectedId = null)
+        {
+            var dao = new ProductDao();
+            ViewBag.ProductID = new SelectList(dao.ListAll(), "ID", "Name", selectedId);
+		}
+        // GET: Admin/Content/Create
+        public ActionResult Create()
+        {
+            SetViewBag();
+            return View();
+        }
+
+        // POST: Admin/Content/Create
+        [HttpPost]
+        public ActionResult Create(Content content)
+        {
+			if (ModelState.IsValid)
+			{
+                var dao = new ContentDao();
+                long id = dao.Insert(content);
+                if (id > 0)
+                {
+                    SetAlert("Thêm mới bài viết thành công !", "success");
+                    return RedirectToAction("Create", "Content");
+                }
+                else
+                {
+                    SetAlert("Thêm mới bài viết thất bại !", "danger");
+                }
+            }
+            SetViewBag();
+            return View("Index");
+        }
+
+        // GET: Admin/Content/Edit/5
+        public ActionResult Edit(long id)
+        {
+            var dao = new ContentDao();
+            var content = dao.GetContentByID(id);
+            SetViewBag(content.ProductID);
+            return View();
+        }
+
+        // POST: Admin/Content/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Content model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            SetViewBag(model.ProductID);
+            return View();
+        }
+
+        // POST: Admin/Content/Delete/5
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                new ContentDao().Delete(id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
